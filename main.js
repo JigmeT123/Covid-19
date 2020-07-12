b586fa49d028c5d80e00df646cc872ee6244ce44
jigmr <jigmetashi02@gmail.com>
2020-07-12 06:11:27 +0400

    2.0
    Upgraded Version

--- main.js
+++ main.js
@@ -1,30 +1,79 @@
-let api="https://api.covid19api.com/total/country/bhutan";
-generateData().then(result=>{
-    console.log(result);
-    let confirmedCase = result.Confirmed;
-    let totalRecovered = result.Recovered;
-    let activeCase = result.Active;
-    let death = result.Deaths;
-    let x = document.querySelector(".card-text1");
-    let y = document.querySelector(".card-text2");
-    let z = document.querySelector(".card-text3");
-    let a = document.querySelector(".card-text4");
-    x.textContent = confirmedCase;
-    x.style.textAlign = "center";
-    x.style.fontSize = "5rem";
-    y.textContent = totalRecovered;
-    y.style.textAlign = "center";
-    y.style.fontSize = "5rem";
-    z.textContent = activeCase;
-    z.style.textAlign = "center";
-    z.style.fontSize = "5rem";
-    a.textContent = death;
-    a.style.textAlign = "center";
-    a.style.fontSize = "5rem";
+mapboxgl.accessToken = 'pk.eyJ1IjoiamlnbWUiLCJhIjoiY2tjOTM5a3EzMWhkMjJ5bWc0ZjRrazA4NyJ9.7gVQ-PWLYFWm7RSNcqj0gg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
+    center: [0, 0], // starting position [lng, lat]
+    minZoom: 1.2,
+    zoom: 1,
+    attributionControl: false // starting zoom
+});
 
-})
-async function generateData(){
-    let response = await fetch(api);
-    let json = await response.json();
-    return json[json.length - 1];
+  var nav = new mapboxgl.NavigationControl({
+    visualizePitch: true
+  });
+map.addControl(nav, 'top-left');
+
+let url = "https://corona.lmao.ninja/v2/countries?&sort";
+
+async function fetchCoronaData() {
+    try {
+        let response = await fetch(url);
+        let json = await response.json();
+        return json;
+    } catch (error) {
+        console.log('There is an error while catching the data');
+    }
 }
+fetchCoronaData().then(result => {
+    console.log(result);
+    result.forEach(res => {
+        let latitude = res.countryInfo.lat;
+        let longitude = res.countryInfo.long;
+        let flag = res.countryInfo.flag;
+        let cases = res.cases;
+        let activeCases = res.active;
+        let country = res.country;
+        //default marker;
+        // var marker = new mapboxgl.Marker({     color: '#ff00ff',     size: 1 })
+        // .setLngLat([ longitude, latitude])   .addTo(map)   .setPopup(new
+        // mapboxgl.Popup({     closeOnMove: true   })   .setHTML(`<p>${country} has
+        // ${activeCases} number of active cases</p>`));
+        
+        var geojson = {
+            type: 'FeatureCollection',
+            features: [{
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+              },
+              properties: {
+                title: country,
+                description: `<br><img src="${flag}"> <br>Total Cases: ${cases} <br> active cases: ${activeCases}<br>`,
+              }
+            }]
+          };
+        
+        geojson.features.forEach(marker =>{
+            var el = document.createElement('div');
+            el.className = 'marker';
+            
+        if(cases > 50000){
+            el.style.backgroundColor = '#ff006f9f';
+            el.style.border = '#ff006e'
+        };
+            new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .setPopup(new mapboxgl.Popup({ 
+                offset: 25,
+                maxWidth: 500,
+                maxHeight: 500
+            })
+            .setHTML('<h2>' + marker.properties.title + '</h2> <h3>' + marker.properties.description + '</h3>'))
+            .addTo(map);
+        })
+        
+    });
+    
+});
+
